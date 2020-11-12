cde91f4e1dbbf756101ec033a0cb5f3ea259a44a
VenkatKumari <venki.kumari@gmail.com>
2020-11-12 01:27:00 +0100

    Updated Input Slider

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -12,7 +12,7 @@ function App() {
   //state
 
   const [songs, setSongs] = useState(data());
-  const [currentSong, setCurrentSong] = useState(songs[6]);
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
 
   return (
